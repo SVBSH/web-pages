a65869949cc42d20814a9073bc34d1c54af2a3a3
SVBSH <getenv@protonmail.com>
2021-06-22 16:06:06 +0200

    add slide galery and transitions

--- room-homepage-master/js/app.js
+++ room-homepage-master/js/app.js
@@ -1,39 +1,55 @@
-const navAngles = document.querySelector('.main-nav__angles');
-const buttonLeft = navAngles.querySelector('.arrow__btn--left');
-const buttonRight = navAngles.querySelector('.arrow__btn--right');
+const navAngles = document.querySelector(".galery__btn-container");
+const buttonLeft = navAngles.querySelector(".galery__btn--left");
+const buttonRight = navAngles.querySelector(".galery__btn--right");
 
-const offers = document.querySelector('.main__offer-container'); 
+const galery = document.querySelector('.galery__list');
 
-const changeOffer = () => {
+const offers = document.querySelector(".main__offer-container");
 
-}
+const changeOffer = (currentOffer, targetOffer) => {
+    currentOffer.classList.remove("main__offer--active");
+    targetOffer.classList.add("main__offer--active");
+};
 
-navAngles.addEventListener('click', e => {
-    const targetBtn = e.target.closest('.arrow__btn');
-    // Left arrow
-    if (targetBtn.classList.contains('arrow__btn--left')) {
-        console.log('left');
 
-        const currentOffer = offers.querySelector('.main__offer.active');
-        const prevOffer = currentOffer.previousElementSibling;
-        if (!prevOffer) {
-            return;
-        }
-        
-        currentOffer.classList.remove('active');
-        prevOffer.classList.add('active');
+navAngles.addEventListener("click", (e) => {
+  const targetBtn = e.target.closest(".galery__btn");
+  const currentOffer = offers.querySelector(".main__offer.main__offer--active");
 
-    // Right arrow
-    } else {
-        console.log('right');
-
-        const currentOffer = offers.querySelector('.main__offer.active');
-        const nextOffer = currentOffer.nextElementSibling;
-        if (!nextOffer) {
-            return;
-        }
-
-        currentOffer.classList.remove('active');
-        nextOffer.classList.add('active');
+  const currentImage = galery.querySelector('.galery__item--active');
+  // Left arrow
+  if (targetBtn.classList.contains("galery__btn--left")) {
+    // const targetOffer = currentOffer.previousElementSibling;
+    // if (!targetOffer) {
+    //     return;
+    // }
+    // changeOffer(currentOffer, targetOffer);
+    console.log("left");
+    const prevOffer = currentOffer.previousElementSibling;
+    if (!prevOffer) {
+        return;
     }
+    currentOffer.classList.remove("main__offer--active");
+    prevOffer.classList.add("main__offer--active");
+
+    // Change image
+    const prevImg = currentImage.previousElementSibling;
+    currentImage.classList.remove('galery__item--active');
+    prevImg.classList.add('galery__item--active');
+  // Right arrow
+  } else {
+    console.log("right");
+    const nextOffer = currentOffer.nextElementSibling;
+    if (!nextOffer) {
+      return;
+    }
+
+    currentOffer.classList.remove("main__offer--active");
+    nextOffer.classList.add("main__offer--active");
+
+    // Change image
+    const nextImg = currentImage.nextElementSibling;
+    currentImage.classList.remove('galery__item--active');
+    nextImg.classList.add('galery__item--active');
+  }
 });
