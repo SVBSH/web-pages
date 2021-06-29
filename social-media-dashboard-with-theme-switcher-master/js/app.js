42d1fc4f9af771d02711353ebae19b23789f6d88
SVBSH <getenv@protonmail.com>
2021-06-29 23:49:23 +0200

    update

--- social-media-dashboard-with-theme-switcher-master/js/app.js
+++ social-media-dashboard-with-theme-switcher-master/js/app.js
@@ -1,15 +1,17 @@
 const changeThemeBtn = document.querySelector("#switch");
-var storedTheme = localStorage.getItem('theme');
-if (storedTheme)
-    document.documentElement.setAttribute('data-theme', storedTheme)
 
+var storedTheme = localStorage.getItem('theme') || 
+    (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) {
+  document.documentElement.setAttribute("data-theme", storedTheme);
+}
 
-changeThemeBtn.addEventListener("click", e => {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  var targetTheme = 'light';
+changeThemeBtn.addEventListener("click", (e) => {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  var targetTheme = "light";
   if (currentTheme === "light") {
-    targetTheme = 'dark';
-  } 
-  document.documentElement.setAttribute('data-theme', targetTheme);
-  localStorage.setItem('theme', targetTheme);
+    targetTheme = "dark";
+  }
+  document.documentElement.setAttribute("data-theme", targetTheme);
+  localStorage.setItem("theme", targetTheme);
 });
