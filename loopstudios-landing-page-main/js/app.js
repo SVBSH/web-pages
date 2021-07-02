32b0de286ed9ebfdf07e2547e398c16fe53c28f2
SVBSH <getenv@protonmail.com>
2021-07-02 16:51:20 +0200

    update

--- loopstudios-landing-page-main/js/app.js
+++ loopstudios-landing-page-main/js/app.js
@@ -6,9 +6,9 @@ const navList = document.querySelector('.main-nav__list');
 hamburger.addEventListener("click", (e) => {
   // Set menu icon
   if (mainNav.classList.toggle('main-nav--open')) {
-    hamburger.src = '../images/icon-close.svg';
+    hamburger.src = 'images/icon-close.svg';
   } else {
-    hamburger.src = '../images/icon-hamburger.svg';
+    hamburger.src = 'images/icon-hamburger.svg';
   }
   // Run animation on menu
   navList.classList.toggle('tracking-in-expand');
