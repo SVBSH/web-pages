5a89af9f5052a150844b7d9587a5a649aaf78f89
SVBSH <getenv@protonmail.com>
2021-07-20 12:18:22 +0200

    Update

--- junior/easybank-landing-page-master/js/app.js
+++ junior/easybank-landing-page-master/js/app.js
@@ -1,3 +1,14 @@
+const hamburger = document.querySelector('.hamburger');
+const menuCross = document.querySelector('.menu-closed');
+const mainNav = document.querySelector('.nav');
+
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("hello");
+    document.querySelector('.menu-control').addEventListener('click', () => toggleNav());
 });
+
+
+function toggleNav() {
+    hamburger.classList.toggle('is-hidden');
+    menuCross.classList.toggle('is-hidden');
+    mainNav.classList.toggle('nav--active');
+}
